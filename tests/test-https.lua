2a320f27b0a3b8ed285a3243bf22b068b4a19312
Ryan Liptak <squeek502@hotmail.com>
2019-04-16 21:20:37 -0700

    Add simple tests

---
+++ tests/test-https.lua
@@ -0,0 +1,21 @@
+local luv = require 'luv'
+local http = require 'coro-http-luv'
+
+coroutine.wrap(function()
+  local res, data = http.request('GET', 'https://github.com/')
+  assert(res and res.code == 200)
+  assert(data)
+
+  print(res.code)
+  for i = 1, #res do
+    local key, value = unpack(res[i])
+    print(key, value)
+  end
+  print(#data)
+
+  local connection = http.getConnection('github.com', 443, true)
+  assert(connection)
+  connection.socket:close()
+end)()
+
+luv.run()
